71ca104a083a2092937a1fe1f2061197fc244827
JML-dev-99 <ganplay0@gmail.com>
2024-10-08 15:05:01 +0800

    Update service-worker.js

--- JS/service-worker.js
+++ JS/service-worker.js
@@ -2,18 +2,11 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('my-pwa-cache').then((cache) => {
             return cache.addAll([
-                '/PWA_FI_PAGE_DEMO/index.html',  // Start from root
-                '/PWA_FI_PAGE_DEMO/Css/Mystyle.css',
-                '/PWA_FI_PAGE_DEMO/JS/app.js',
-                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-72x72.png',
-                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-96x96.png',
-                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-128x128.png',
-                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-144x144.png',
-                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-152x152.png',
-                '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-192x192.png',
-                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-384x384.png',
-                '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-512x512.png',
-                
+                  './index.html',
+                './Css/Mystyle.css',
+                './JS/app.js',
+                './Asset/Icon/icon-192x192.png',
+                './Asset/Icon/icon-512x512.png',
             ]);
         })
     );
