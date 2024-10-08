28e666a8efc1fa8889ca217f8d7fbba363cd8332
JML-dev-99 <ganplay0@gmail.com>
2024-10-08 12:55:42 +0800

    Add files via upload

---
+++ JS/service-worker.js
@@ -0,0 +1,21 @@
+﻿self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('my-pwa-cache').then((cache) => {
+            return cache.addAll([
+                './index.html',
+                './Css/Mystyle.css',
+                './JS/app.js',
+                './Asset/Icon/icon-192x192.png',
+                './Asset/Icon/icon-512x512.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
