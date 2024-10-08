9506ab802946258b83ee86014a331cae9ec4e546
JML-dev-99 <ganplay0@gmail.com>
2024-10-08 17:00:11 +0800

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -11,3 +11,29 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-info bar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later
+    deferredPrompt = e;
+    // Update your UI to notify the user they can add to home screen
+    showInstallButton(); // Create a function to show an install button
+});
+
+document.getElementById('installButton').addEventListener('click', () => {
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+        } else {
+            console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
+});
+
