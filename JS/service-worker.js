a961263de28d761c888aefd07c2906d818ecd360
JML-dev-99 <ganplay0@gmail.com>
2024-10-08 14:47:53 +0800

    Update service-worker.js

--- JS/service-worker.js
+++ JS/service-worker.js
@@ -5,8 +5,15 @@ self.addEventListener('install', (event) => {
                 '/PWA_FI_PAGE_DEMO/index.html',  // Start from root
                 '/PWA_FI_PAGE_DEMO/Css/Mystyle.css',
                 '/PWA_FI_PAGE_DEMO/JS/app.js',
+                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-72x72.png',
+                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-96x96.png',
+                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-128x128.png',
+                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-144x144.png',
+                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-152x152.png',
                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-192x192.png',
+                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-384x384.png',
                 '/PWA_FI_PAGE_DEMO/Asset/Icon/icon-512x512.png',
+                
             ]);
         })
     );
